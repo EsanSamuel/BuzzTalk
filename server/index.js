50496f44da962aba2cc6c9ca62451c8dfc1856df
Esan Samuel <esansamuel555@gmail.com>
2023-08-11 15:45:09 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,9 @@ const io = new Server(server, {
         methods: ['GET', 'POST'],
     }
 })
-app.use(cors())
+app.use(cors({
+    origin: 'https://buzz-talk-xi.vercel.app/'
+}))
 app.use(express.json({ limit: '50mb' }))
 
 app.use('/api/v1/post', postRoutes)
