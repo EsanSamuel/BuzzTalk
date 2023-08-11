5c4055da2cb95529a79a0f931b654fec3228b504
Esan Samuel <esansamuel555@gmail.com>
2023-08-11 16:36:05 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ import http from 'http'
 dotenv.config()
 
 const app = express()
+app.use(express.static('public'));
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
