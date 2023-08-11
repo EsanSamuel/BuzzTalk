032c97fd3fbd83ee794c8aac95632d179e74e8f1
Esan Samuel <esansamuel555@gmail.com>
2023-08-11 15:46:01 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,12 +15,12 @@ const app = express()
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://buzz-talk-xi.vercel.app',
         methods: ['GET', 'POST'],
     }
 })
 app.use(cors({
-    origin: 'https://buzz-talk-xi.vercel.app/'
+    origin: 'https://buzz-talk-xi.vercel.app'
 }))
 app.use(express.json({ limit: '50mb' }))
 
